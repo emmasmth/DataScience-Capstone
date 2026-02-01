e9b80a49dbe11d5f3eced7da613cdeb27ae2f036
emmasmth <edsmith1@loyola.edu>
2026-02-01 18:45:41 -0500

    update gitignore and main. add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# DataScience-Capstone
+
+Emma Smith
+
+Dr. Bu Hyoung Lee
+
+May 2026
+
+DS796 Data Science Project
+
+## A New American Dream: Analyzing and Predicting Retirement Trends in America
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello")
+from src.dataset import *
+from src.processor import *
+
+"""
+A New American Dream: Analyzing and Predicting Retirement Trends in America
+"""
+
+file_path = "./data/randhrs1992_2022v1.sas7bdat"
+data = DataSet(file_path)
+processor = Processor(data.df)
+processor.plot_waves()
+processor.plot_age()
